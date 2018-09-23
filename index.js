89f521b9b67eb5149095ca5fbc3ea63708be082f
Zac Willmington <zac.willmington@gmail.com>
2018-09-23 17:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 $(document).ready(function (){
+    Handlebars.
 });
+
+
+function searchRepositories() {
+    const searchTerm = document.getElementById('searchTerms').value;
+    const searchUrl = `https://api.github.com/search/repositories?q=${searchTerm}`;
+    $.get(searchUrl, function(response) {
+        displayRepositories(response);
+    });
+}
+
+function displayRepositories(repos) {
+
+    
+}
+
+function displayError() {
+
+}
