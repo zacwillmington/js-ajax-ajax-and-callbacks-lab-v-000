d67360e7f46d15ec4e29353eedacf38bf91c0a45
Zac Willmington <zac.willmington@gmail.com>
2018-09-23 22:36:21 +0000

    Done.

--- index.js
+++ index.js
@@ -3,31 +3,54 @@ $(document).ready(function (){
 });
 
 
-function searchRepositories() {
+function searchRepositories(response) {
+
     const searchTerm = document.getElementById('searchTerms').value;
     const searchUrl = `https://api.github.com/search/repositories?q=${searchTerm}`;
+
     $.get(searchUrl, function(response) {
-        displayRepositories(response);
+        $('#results').html(displayRepositories(response));
+    }).fail(function(error){
+        displayError();
     });
 }
 
 function displayRepositories(repos) {
-    debugger;
+
     let reposList = '<ul>' + `${repos.items.map(function(repo) {
-            <li>repo.name</li>
-            <a href="#" onclick="showCommits();">See Commits</a>
+            return '<li>' + repo.name  +
+            '<a href="#" data-usrName=' + repo.owner.login + ' data-repo="'+ repo.name +'" onclick="showCommits(this);">See Commits</a>' + '</li>'
         }
-    )};`
-    //repositoryTemplateFn(repos)
+    ).join('')}` + '</ul>';
+    return reposList;
 
 }
 
-function showCommits() {
-    debugger;
+function showCommits(repo) {
+
+    // const repoName = repo.dataset.repo;
+    // const userName = repo.dataset.usrname;
+    const userName = repo.dataset.owner;
+    const repoName = repo.dataset.repository;
+
+    const searchUrl = `https://api.github.com/repos/${userName}/${repoName}/commits`
+    $.get(searchUrl, function(response) {
+        $('#details').html(displaycommits(response));
+    }).fail(function(error){
+        displayError();
+    });
+}
+
+function displaycommits(commits){
+    let commitsList = '<ul>' + `${commits.map(function(commit) {
+            return '<li>' + commit.sha + '</li>'
+        }
+    ).join('')}` + '</ul>';
+    return commitsList;
 
 }
 
 
 function displayError() {
-
+    $('#errors').append("<p>/error/</p>");
 }
